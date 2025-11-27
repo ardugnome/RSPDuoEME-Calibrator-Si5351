217da2e485a2a9f15a59edbcc35762670eeedaa0
ardugnome <37820798+ardugnome@users.noreply.github.com>
2025-11-27 00:22:47 -0600

    Refactor Si5351 calibration routine
    Updated frequency variable and removed unused functions.

--- Si5351.ino
+++ Si5351.ino
@@ -1,89 +1,40 @@
+/*
+Calibration routine for RSPDuoEME using Si5351 and Arduino (Nano) 
+Connections SDA-A4 SCL-A5
+Choose COM port in the RSPDuoEME and choose Auto Claibration
+Hit Start and wait 5 seconds, if necessary repeat and click Set
+Si5351 shuts down after 5 seconds
+Nov 2025
+*/
+
 #include "si5351.h"
 #include "Wire.h"
-#include <avr/sleep.h>
-#include <avr/power.h>
-#include <avr/wdt.h> 
+#define SI5351_REF 25002650UL
 
 Si5351 si5351;
 
-unsigned long long freq  = 14414950000ULL; // 211.600000MHz x 49 = 10368.4Mhz
+unsigned long long freq  = 14414500000ULL; 
 
 void setup() {
-//  si5351.set_correction(+900000, SI5351_PLL_INPUT_XO);
-  si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);            
-  si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_4MA);  
-  si5351.set_freq(freq, SI5351_CLK0);                    
-  delay(3000);
-  si5351.output_enable(SI5351_CLK0, 0); // Disable the output
-  delay(1000);
-  i2c_switch_off();
-  Manual_LowPower_Mode(1);   
- set_sleep_mode (SLEEP_MODE_PWR_DOWN);
- Deep_Sleep_Manual();
-  
+  Serial.begin(9600);
+si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_2MA); 
+  si5351.init(SI5351_CRYSTAL_LOAD_8PF, SI5351_REF, 0);          
 }
 
 void loop() {
-    
+    if (Serial.available()) {
+  unsigned long input=Serial.parseInt();  // Read until newline
+ freq = input*1000;
+     freq=freq*100;
+     si5351.set_freq(freq, SI5351_CLK0);     
+  si5351.output_enable(SI5351_CLK0, 1);           
+  delay(5000);
+  si5351.output_enable(SI5351_CLK0, 0); // Disable the output
+  //delay(1000);
 
-  // Power down the specific clock output using a register command
-  // The command below sets the CLK0_PDN bit in the appropriate register
-  // Consult the Si5351 datasheet or library source code for exact register/bit values
-  // A common method with libraries is:
-
- 
-}
-void i2c_switch_off(){
-
-  // turn off I2C
-
-  TWCR &= ~(bit(TWEN) | bit(TWIE) | bit(TWEA));
-
-
-  // turn off I2C pull-ups
-
-  digitalWrite (A4, LOW);
-
-  digitalWrite (A5, LOW);
-
-}
-void Deep_Sleep_Manual(){
-
-  // CORE State
-
-  set_sleep_mode (SLEEP_MODE_PWR_DOWN);
-
-  ADCSRA = 0;            // turn off ADC
-
-  power_all_disable ();  // power off ADC, Timer 0 and 1, serial interface
-
-
-  // Interrupts are not counted as ADXL require
-
-  noInterrupts ();       // timed sequence coming up
-
-  sleep_enable ();       // ready to sleep
-
-  interrupts ();         // interrupts are required now
-
-  sleep_cpu ();          // sleep                
-
-  sleep_disable ();      // precaution
-
-  power_all_enable ();   // power everything back on
-
-  
-
-}  // end of goToSleep 
-
-void Manual_LowPower_Mode(uint8_t multiplier){
-
-  delay(70);                                              // Requires at least 68ms of buffer head time for module booting time
-
-  for(int i = 0; i <= multiplier; i++){                   // Multiplier for Power Down Tick
-
-    Deep_Sleep_Manual();
+    input = "";
+    freq="";
 
   }
-
+ 
 }
